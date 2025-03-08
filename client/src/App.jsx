4871bd2e0ed217ac5e2dd8fe9d33a5ea3f1d22e7
Tushar <tusharjain2308@gmail.com>
2025-03-08 22:48:14 +0530

    created client routes for different pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx"
+import SignIn from "./pages/SignIn.jsx"
+import SignUp from "./pages/SignUp.jsx"
+import Projects from "./pages/Projects.jsx"
+import Dashboard from "./pages/Dashboard.jsx"
 
-const App = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
-};
-
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
